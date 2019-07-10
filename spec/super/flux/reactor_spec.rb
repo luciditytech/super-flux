c3c930c51aa4d662ad1aaa64874cb1d29b792de7
Marcelo Wiermann <marcelo.wiermann@gmail.com>
2019-07-10 09:23:09 -0700

    chore(*): adds additional reactor specs

--- spec/super/flux/reactor_spec.rb
+++ spec/super/flux/reactor_spec.rb
@@ -13,6 +13,28 @@ RSpec.describe Super::Flux::Reactor do
     let(:consumer) { double }
     let(:message) { double(topic: topics.first) }
 
+    shared_examples_for 'a reactor loop' do
+      it 'subscribes do all topics' do
+        topics[0..-2].each do |topic|
+          expect(consumer).to receive(:subscribe).with(topic)
+        end
+
+        subject
+      end
+
+      it 'throttles the message' do
+        expect(Super::Flux::Governor).to receive(:call).with(message, 0)
+        subject
+      end
+
+      it 'executes the task' do
+        expect(Super::Flux::ExecuteTask).to receive(:call)
+          .with(task, message)
+
+        subject
+      end
+    end
+
     before do
       allow(Super::Flux).to receive(:consumer).and_return(consumer)
       allow(task).to receive(:settings).and_return(settings)
@@ -24,20 +46,36 @@ RSpec.describe Super::Flux::Reactor do
       end
 
       allow(Super::Flux::Governor).to receive(:call)
-      allow(Super::Flux::ExecuteTask).to receive(:call).and_return(true)
     end
 
-    it 'subscribes do all topics' do
-      topics[0..-2].each do |topic|
-        expect(consumer).to receive(:subscribe).with(topic)
+    context 'when the task execution is successful' do
+      before do
+        allow(Super::Flux::ExecuteTask).to receive(:call).and_return(true)
       end
 
-      subject
+      it_behaves_like 'a reactor loop'
+
+      it 'does not attempt a retry' do
+        expect(Super::Flux::RetryTask).to_not receive(:call)
+
+        subject
+      end
     end
 
-    it 'throttles the message' do
-      expect(Super::Flux::Governor).to receive(:call).with(message, 0)
-      subject
+    context 'when the task execution fails' do
+      before do
+        allow(Super::Flux::ExecuteTask).to receive(:call).and_return(false)
+        allow(Super::Flux::RetryTask).to receive(:call).and_return(true)
+      end
+
+      it_behaves_like 'a reactor loop'
+
+      it 'attempts a retry' do
+        expect(Super::Flux::RetryTask).to receive(:call)
+          .with(message, topics[1])
+
+        subject
+      end
     end
   end
 end
